4b6dc725467fb41915d97357db735517642b1e98
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-09-22 19:07:42 -0400

    Done

--- swift-unit-conversions/AppDelegate.swift
+++ swift-unit-conversions/AppDelegate.swift
@@ -9,16 +9,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         
-        /**
-
-        * Call your functions here!
-
-        */
+        let sandbox = Sandbox()
         
-        // do not alter
-        return true  //
-    }   ///////////////
-}       ///////////////
-
+        let fahrenheitFromCelsius = sandbox.fahrenheitFromCelsius(45)
+        print(fahrenheitFromCelsius)
+        
+        let celsiusFromFahrenheit = sandbox.celsiusFromFahrenheit(45)
+        print(celsiusFromFahrenheit)
+        
+        
+        let radiansFromDegrees = sandbox.radiansFromDegrees(45)
+        print(radiansFromDegrees)
+        
+        let degreesFromRadians = sandbox.degreesFromRadians(45)
+        print(degreesFromRadians)
+        
+        let milesFromKilometers = sandbox.milesFromKilometers(45)
+        print(milesFromKilometers)
+        
+        let kilometersFromMiles = sandbox.kilometersFromMiles(45)
+        print(kilometersFromMiles)
+        
+        return true
+    }
+} 
--- swift-unit-conversions/Sandbox.swift
+++ swift-unit-conversions/Sandbox.swift
@@ -2,11 +2,37 @@
 
 import Foundation
 
-/**
-
-* Write your functions here!
-
-*/
-
-
+class Sandbox {
+    
+    func fahrenheitFromCelsius(celsius: Double) {
+        
+        let toFahrenheit = (celsius * 1.8 + 32.0)
+        
+        print(toFahrenheit)
+        
+    }
+    
+    func celsiusFromFahrenheit(fahrenheit: Double) -> Double{
+        var toCelsius = (fahrenheit - 32.0) * 0.5556
+        print(toCelsius)
+        return toCelsius
+    }
+    
+    func radiansFromDegrees(degrees: Double) -> Double {
+        
+        return (degrees * M_PI)/180
+    }
+    func degreesFromRadians(radians: Double) -> Double {
+        return (radians * 180) / M_PI
+    }
+    
+    func milesFromKilometers(kilometers: Double) -> Double {
+        return kilometers * 0.62137
+    }
+    
+    func kilometersFromMiles(miles: Double) -> Double {
+        return miles * 1.609344
+    }
+    
+}
 
